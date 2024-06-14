f90eda37e0e84fa059b5afadf9fd8f3ab7be445c
Nilu Isakova <niluisakova@gmail.com>
2024-06-14 15:26:36 -0500

    lexer v2

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,6 @@ import java.util.List;
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
-
-
     public static void main(String[] args) {
         List<Token> tokens = Lexer.getHardcodedTokens();
         Parser parser = new Parser(tokens);
