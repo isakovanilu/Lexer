ca0be60ab15660144a50e44527dfad6a67ea7e3f
Nilu Isakova <niluisakova@gmail.com>
2024-07-04 23:38:35 -0500

    lexer v2

---
+++ src/Block.java
@@ -0,0 +1,17 @@
+
+import java.util.List;
+
+public class Block extends ASTNode {
+    List<ASTNode> statements;
+
+    Block(List<ASTNode> statements) {
+        this.statements = statements;
+    }
+    @Override
+    public void print(String indent) {
+        for (ASTNode node: statements) {
+            node.print(" ");
+        }
+
+    }
+}
